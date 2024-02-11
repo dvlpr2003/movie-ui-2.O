2aef934d0bb04d9971bf38da778e1fba4fce37f9
dvlpr2003 <gayathrigaya698@gmail.com>
2024-02-11 07:56:43 -0800

    overView

--- src/movie.js
+++ src/movie.js
@@ -1,41 +1,64 @@
+import { useState } from "react"
 import "./movie.css"
+import Overview from "./Overview"
 
-export default function Movie({Series,error,Load}){
+export default function Movie({Series,Load}){
+    const [Name,setName]=useState([])
     return (
         <div id="movie">
-            <RenderMovie Series={Series} error={error} Load={Load}/>
-            <Rating/>
+            <RenderMovie Series={Series}  Load={Load} setName={setName} Name={Name}/>
+            <Overview/>
         </div>
     )
 }
-function RenderMovie({Series,error,Load}){
+function RenderMovie({Series,Load,setName,Name}){
     return(
         <div className="movie-component" >
             {Load && <Loading/>}
-            { Series?<List Series={Series}/>: <Found/>}
-
+            { Series?<List Series={Series} setName={setName} Name={Name}/>: <Found/>}
         </div>
     )
 }
-function List({Series}){
+function List({Series,setName,Name}){
+
     return(
         <>
-    { Series.map((e)=>
-    <div className="list">
-        <div id="list-img">
-            <img src={e.Poster} alt={e.Title}/>
-        </div>
-        <div id="move-name-year">
-            <span>{e.Title}</span>
-            <span>🗓️ {e.Year}</span>
-
-        </div>
-
-    </div>)
+    { Series.map((e)=><Items Title ={e.Title} Poster={e.Poster} Year={e.Year} setName={setName} Name={Name}/>)
 
     }
     </>
 
+    )
+}
+function Items({Title,Poster,Year,setName,Name}){
+    const Ftc ={
+        Title:Title,
+        Poster:Poster,
+        Year:Year
+    }
+    function getName(){
+        let Check = Name.findIndex((it)=>it.Title===Ftc.Title)
+        // if the condition is satisfied then it will returns 3 ,otherwise it returns -1
+        if(Check == -1){
+            setName((e)=>[...e,Ftc])
+
+        }
+        else{
+            setName((e)=>[...e])
+        }
+    }
+    return(
+        <div className="list" onClick={getName}>
+        <div id="list-img">
+            <img src={Poster} alt={Title}/>
+        </div>
+        <div id="move-name-year">
+            <span>{Title}</span>
+            <span>🗓️ {Year}</span>
+
+        </div>
+
+    </div>
     )
 }
 function Found(){
@@ -51,12 +74,5 @@ function Loading(){
         <span style={{color:"white"}}>Loading . . .</span>
         </div>
 
-    )
-}
-function Rating(){
-    return(
-        <div className="movie-component">
-
-        </div>
     )
 }
